6011131cb0d2d38caa0a0dfb1bb9e41a30494f95
hannakoshy3 <hmk4.njit.edu>
2024-06-10 14:01:23 -0400

    solved problem 1

--- public_html/M2/problem1.php
+++ public_html/M2/problem1.php
@@ -9,6 +9,8 @@ function processArray($arr) {
     //start edits
     //note: use the $arr variable to iterate over, don't directly touch $a1-$a4
     //TODO add logic here to echo out only odd values
+    //hmk4 6-10-24
+    echo "Printing the odd values only...";
     //end edits
 }
 echo "Problem 1: Odd Output<br>";
