ea0037a5831a905c4f2cb14f81fd7bf5ca47f7f0
hannakoshy3 <hmk4.njit.edu>
2024-07-14 17:04:35 -0400

    logout

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,15 +1,12 @@
 <?php
-require(__DIR__ . "/../../partials/nav.php");
+require(__DIR__."/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
-
-if (is_logged_in(true)) {
-    //echo "Welcome home, " . get_username();
-    //comment this out if you don't want to see the session variables
-    error_log("Session data: " . var_export($_SESSION, true));
+if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
+ echo "Welcome, " . $_SESSION["user"]["email"]; 
 }
-?>
-<?php
-require(__DIR__ . "/../../partials/flash.php");
+else{
+  echo "You're not logged in";
+}
 ?>
