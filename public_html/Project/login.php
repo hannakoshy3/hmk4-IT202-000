f038e18579bf00d6d0808ab8def65eb6b880dcd1
hannakoshy3 <hmk4.njit.edu>
2024-07-14 18:15:16 -0400

    updating files again

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -33,10 +33,19 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         $hasError = true;
     }
     //sanitize
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = sanitize_email($email);
     //validate
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Invalid email address";
+    /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Please enter a valid email <br>";
+        $hasError = true;
+    }
+    if (empty($password)) {
+        echo "Password must be provided <br>";
+        $hasError = true;
+    }*/
+    if (!is_valid_email($email)) {
+        echo "Please enter a valid email <br>";
         $hasError = true;
     }
     if (empty($password)) {
